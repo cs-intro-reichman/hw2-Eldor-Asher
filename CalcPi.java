f8b20e012e45aa5da3c9b1f371335f946cd042cd
Eldor Asher <eldorasher@MacBook-Air-sl-Eldor.local>
2025-11-09 13:35:33 +0200

    CalcPi

--- CalcPi.java
+++ CalcPi.java
@@ -4,22 +4,22 @@ public class CalcPi {
                         int number = Integer.parseInt(args[0]);
                         int numerator = 1;
                         int denominator = 1;
-                        float sum =0;
+                        double sum =0;
                         for (int j=0; j<number; j++)
                         {
                                     if (j % 2 == 0)
                                     {
-                                                sum += (float) numerator/denominator;
+                                                sum += (double) numerator/denominator;
                                                 denominator +=2;
                                     }
                                     else
                                     {
-                                                sum -= (float) numerator/denominator;
+                                                sum -= (double) numerator/denominator;
                                                 denominator +=2;
                                     }
                         }
                         System.out.println("pi according to Java: " + Math.PI);
-                        System.out.println("pi, approximated:     " + (float) sum * 4);
+                        System.out.println("pi, approximated:     " + (double) sum * 4);
                 // Replac this comment with your code
             }
 }
