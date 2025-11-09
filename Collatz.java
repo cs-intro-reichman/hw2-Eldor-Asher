9479f606ea01e27dfb63ca92ada543201584d43e
Eldor Asher <eldorasher@MacBook-Air-sl-Eldor.local>
2025-11-09 14:00:51 +0200

    Collatz.java

--- Collatz.java
+++ Collatz.java
@@ -1,44 +1,40 @@
  
  
-public class Collatz {
-            public static void main(String args[]) {
-                // Replace this comment with your code
-                        int N = Integer.parseInt(args[0]);
-                        String letter = args[1];
-                        String sequence = "";
-                        int counter = 1;
-                        int number = 1;
-                        boolean flag = true;
-                        for (int i = 1; i <= N; i++)
-                        {
-                                    while (flag)
-                                    {
-                                                sequence += " " +String.valueOf(number);
-                                                if (number % 2 == 0)
-                                                {
-                                                            number = number / 2;
-                                                }
-                                                else
-                                                {
-                                                            number = number * 3 + 1;
-                                                }
-                                                counter +=1;
-                                                if (number == 1)
-                                                {
-                                                            flag = false;
-                                                            sequence+= " " +String.valueOf(number);
-                                                }
-                                               
-                                    }
-                                    number = i +1;
-                                    flag = true;
-                                    if (letter.equals(""+ "v"))
-                                    {
-                                                System.out.println(sequence + " (" + counter +")");
-                                    }
-                                    counter =1;
-                                    sequence = "";          
-                        }
-                        System.out.println(" Every one of the first " + N + " hailstone sequences reached 1");
+public class Collatz {public static void main(String args[]) {
+    int N = Integer.parseInt(args[0]);
+    String letter = args[1];
+
+    for (int i = 1; i <= N; i++) {
+        int number = i;
+        int counter = 1;
+        String sequence = "";
+
+        while (true) {
+            // הוספת המספר לרצף בלי רווח מיותר בהתחלה
+            if (sequence.equals("")) {
+                sequence = "" + number;
+            } else {
+                sequence += " " + number;
             }
+
+            if (number == 1) {
+                break;
+            }
+
+            if (number % 2 == 0) {
+                number = number / 2;
+            } else {
+                number = number * 3 + 1;
+            }
+
+            counter++;
+        }
+
+        if (letter.equals("v")) {
+            System.out.println(sequence + " (" + counter + ")");
+        }
+    }
+
+    System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
+}
 }
