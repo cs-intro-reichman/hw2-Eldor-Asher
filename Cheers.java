9dc10e32eb1ed23d1d7e35251c3c054045396752
Eldor Asher <eldorasher@MacBook-Air-sl-Eldor.local>
2025-11-09 10:42:26 +0200

    Cheers.java

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,34 @@
-//feedback
 
+//feedback
+ 
 /*
 Great work boaz!
 */
-
-
+ 
+ 
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String word = args[0];
+                int number = Integer.parseInt(args[1]);
+                int i=0;
+                String letters = "AEFHILMNORSX";
+                while (i < word.length())
+                {
+                        char c = word.charAt(i);
+                        if (letters.contains("" + c))
+                        {
+                                System.out.println("give me an " + args[0].charAt(i));
+                        }
+                        else
+                        {
+                                System.out.println("give me a " + args[0].charAt(i));
+                        }
+                        i++;
+                }
+                for (int j =0; j<number ; j++)
+                {
+                        System.out.println(word + "!!!");
+                }
         }
 }
